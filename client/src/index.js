2a14b66e413382bc547cdabd3c693d8bf6add144
Ben Diuguid <bendiuguid@gmail.com>
2017-05-19 12:57:21 -0400

    update dependencies

--- client/src/index.js
+++ client/src/index.js
@@ -1,7 +1,9 @@
 import 'whatwg-fetch';
 import React from 'react';
 import ReactDOM from 'react-dom';
+import registerServiceWorker from './registerServiceWorker';
 import App from './components/App';
 import './index.css';
 
 ReactDOM.render(<App />, document.getElementById('root'));
+registerServiceWorker();
